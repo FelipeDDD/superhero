a7f2015d35ffe7bbe69bf426b798050dfd7d7d98
FelipeDDD <felipedias253@hotmail.com>
2024-06-19 00:18:50 +0200

    Segundo commit

--- script.js
+++ script.js
@@ -12,37 +12,90 @@
 // btn.onclick = () => vaiGato()
 
 // https://superheroapi.com/api/8349c73eccc2c3fc6b1dc8a40ee79359
+// randomHero.onclick = () => {getSuperHero(randomNumber(1,733));}
 
-const getNewHero = document.getElementById("newHeroButton");
-const savedHero = document.getElementById("imgHero");
-
-// getNewHero.onclick = () => {getSuperHero(randomNumber(1,733));}
+const randomHero = document.getElementById("newHeroButton");
+const imgHero = document.getElementById("imgHero");
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
+const heroName = document.getElementById("heroName");
+const str = document.getElementById("strenghtDiv")
+const speed = document.getElementById("speedDiv")
 
 const BASE_URL = `https://superheroapi.com/api.php/8349c73eccc2c3fc6b1dc8a40ee79359/`;
 
-const getSuperHero = (id) => {
-    savedHero.style.display= "block";
-    savedHero.style.width = "400px";
-    savedHero.style.height = "400px";
+const randomSuperHero = (id) => {
+    // savedHero.style.display= "block";
+    // savedHero.style.width = "500px";
+    // savedHero.style.height = "500px";
     
   fetch(`${BASE_URL}/${id}`)
     .then((response) => response.json())
     .then((json) => {
-    //    document.querySelector("body").innerHTML += `<img src="${json.image.url}" height=400 width=400/>`;
-      savedHero.src = json.image.url;
+        // savedHero.src = json.image.url;
+        // heroName.textContent = json.name;
+        // str.textContent = "Strength:" + "  " + json.powerstats.strength;  
+        const superHero = json
+        showHeroInfo(superHero);
     });
 };
-// console.log(getSuperHero(randomNumber(1,733)));
 
+const showHeroInfo = (character) => {
+    const name = `<h2>${character.name}</h2>`
+    const img = `<img src="${character.image.url}" height=500 width=500/>`
+    const pau = `<p>🍆 BELUGA: ${Math.floor(Math.random() * 20 + 1)}CM</p>`
+    
+    
+    const stats = Object.keys(character.powerstats).map(stat => {
+        
+        return `<p>${statToEmoji[stat]} ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
+    }).join('')
+
+    imgHero.innerHTML = `${name}${img}<h3>${stats}${pau}</h3>`
+}
+const statToEmoji = {
+    intelligence: '🧠',
+    strength: '💪',
+    speed: '🏃',
+    durability: '🔋',
+    power: '🏋️',
+    combat: '⚔️',
+}
+
+const searchSuperHero = (name) => {
+    imgHero.style.display= "block";
+    imgHero.style.width = "500px";
+    imgHero.style.height = "500px";
+    
+    fetch(`${BASE_URL}/search/${name}`)
+    .then((response) => response.json())
+    .then((json) => {
+    imgHero.src = json.results[0].image.url;
+    heroName.textContent = json.results[0].name;
+
+    // str.textContent = "Strength:" + "  " + json.results[0].powerstats.strength;
+    // speed.textContent = "Speed:" + "  " + json.results[0].powerstats.speed;
+    // piroca.textContent = "Tamanho do pau:" + " " + Math.floor(Math.random() * 20)
+    })
+}
 function randomNumber(min, max) {
   const randomNumber = Math.random() * (max - min) + min;
   const roundedNumber = Math.floor(randomNumber);
   return roundedNumber;
 }
-
+// function randomNumber2 = () => {
+//     return Math.floor(Math.random() * 733)
+// }
 function functionSeparada() {
-  savedHero.style.display = "block";
-  getSuperHero(randomNumber(1, 733));
+  imgHero.style.display = "block";
+  randomSuperHero(randomNumber(1, 733));
+}
+function searchHero () {
+    const inputValue = searchInput.value;
+    searchSuperHero(inputValue)
 }
 
-getNewHero.addEventListener("click", functionSeparada);
+randomHero.addEventListener("click", functionSeparada);
+searchButton.addEventListener("click", searchHero);
+
+
